8fdf44c7af303fcbbde747569a0a65f4148a994b
kholid46 <budimangess99@gmail.com>
2025-05-25 01:10:33 +0700

    Create detect_custom.py

---
+++ detect_custom.py
@@ -0,0 +1,13 @@
+# detect_custom.py
+import sys
+from pathlib import Path
+
+# Pastikan path YOLOv5 ditambahkan
+FILE = Path(__file__).resolve()
+ROOT = FILE.parent / "yolov5"
+sys.path.append(str(ROOT))
+
+from yolov5.detect import run as detect_run
+
+def run_detection(weights, source, conf=0.25):
+    detect_run(weights=weights, source=source, conf_thres=conf, save_txt=True, save_conf=True)
