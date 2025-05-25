64f52733e3b3ca9afc3b70e461c9dd900c7c05cb
kholid46 <budimangess99@gmail.com>
2025-05-25 14:51:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,24 @@ if uploaded_file is not None:
     st.info("🚀 Proses deteksi berjalan...")
     output_image_path, csv_path, hasil_df = run_detection(input_path)
 
-    if hasil_df is not None and not hasil_df.empty:
+        if hasil_df is not None and not hasil_df.empty:
         st.image(output_image_path, caption="Hasil Deteksi", use_column_width=True)
         st.success("✅ Hilal terdeteksi.")
         st.dataframe(hasil_df)
-        st.download_button("📥 Unduh CSV Deteksi", data=hasil_df.to_csv(index=False), file_name="hasil_deteksi.csv", mime="text/csv")
-    else:
-        st.warning("⚠️ Tidak ada hilal terdeteksi.")
+
+        # Tombol unduh CSV
+        st.download_button(
+            "📥 Unduh CSV Deteksi",
+            data=hasil_df.to_csv(index=False),
+            file_name="hasil_deteksi.csv",
+            mime="text/csv"
+        )
+
+        # Tombol unduh gambar hasil
+        with open(output_image_path, "rb") as img_file:
+            st.download_button(
+                label="📥 Unduh Gambar Hasil Deteksi",
+                data=img_file,
+                file_name="hasil_deteksi.jpg",
+                mime="image/jpeg"
+            )
